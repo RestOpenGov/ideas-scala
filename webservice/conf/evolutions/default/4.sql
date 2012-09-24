e66fcb144b0034773bcdb92c4ae7d46857a5a44b
opensas <opensas@gmail.com>
2012-09-24 15:06:13 -0300

    add recommendation as an idea type

--- webservice/conf/evolutions/default/4.sql
+++ webservice/conf/evolutions/default/4.sql
@@ -3,9 +3,10 @@
 # --- !Ups
 
 INSERT INTO idea_type VALUES
-  (1, 'idea',       'Una propuesta para mejorar la ciudad'),
-  (2, 'reclamo',    ''),
-  (3, 'pregunta',   'Una pregunta acerca de la ciudad para que la comunidad responda')
+  (1, 'idea',           'Una propuesta para mejorar la ciudad'),
+  (2, 'reclamo',        'Algo que no nos gusta de la ciudad y que tenemos que cambiar'),
+  (3, 'pregunta',       'Una pregunta acerca de la ciudad para que la comunidad responda'),
+  (4, 'recomendación',  'Algún lugar o evento de la ciudad que no te podés perder')
 ;
 
 # --- !Downs
