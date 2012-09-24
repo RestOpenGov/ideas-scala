ee6a3555f900dd4758bfbc1143c00c090ae0cf05
opensas <opensas@gmail.com>
2012-09-23 23:53:18 -0300

    refs #16

---
+++ webapp/js/app/controller.js
@@ -0,0 +1,39 @@
+/*globals $,_*/
+'use strict';
+function Controller($scope, $http) {
+
+  $scope.ideas_endpoint = 'http://ideas-jugar.rhcloud.com/api/ideas?order=id';
+  $scope.ideas = {};
+
+  $scope.$on('$viewContentLoaded', function() {
+    console.log('we are ready...');
+  });
+
+  $scope.search = function(){
+    console.log('searching');
+    // Just in case the web service doesn't support X-Requested-With header
+    // delete $http.defaults.headers.common['X-Requested-With']
+    $http.get($scope.ideas_endpoint).success(function(json) {
+      $scope.ideas = json;
+
+      // generate random votes -- mock
+      angular.forEach($scope.ideas, function(value, key) {
+        value.votes = $scope.votes();
+      })
+      console.log($scope.ideas);
+    });
+  };
+
+  $scope.votes = function() {
+    return {
+      pos: $scope.random(100),
+      neg: $scope.random(20)
+    }
+  };
+
+  $scope.random = function(top) {
+    top = top || 100;
+    return Math.floor(Math.random() * top+1);
+  };
+
+};
