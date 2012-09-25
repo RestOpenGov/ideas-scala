f36767febf84c56328c4c9694c86a59f1c24dafd
opensas <opensas@gmail.com>
2012-09-25 13:36:52 -0300

    minimal test for VoteCounter functionality

--- webservice/conf/evolutions/default/10.sql
+++ webservice/conf/evolutions/default/10.sql
@@ -16,9 +16,10 @@ INSERT INTO vote VALUES
 
   (11, 'comment', null, 1, 1, true,  '2012-09-20 07:03:00'),
   (12, 'comment', null, 1, 2, true,  '2012-09-20 11:34:00'),
-  (13, 'comment', null, 2, 4, true,  '2012-09-22 13:43:00'),
-  (14, 'comment', null, 2, 3, false, '2012-09-23 12:56:00'),
-  (15, 'comment', null, 2, 1, false, '2012-09-24 13:02:00')
+  (13, 'comment', null, 1, 3, false, '2012-09-20 11:34:00'),
+  (14, 'comment', null, 2, 4, true,  '2012-09-22 13:43:00'),
+  (15, 'comment', null, 2, 3, false, '2012-09-23 12:56:00'),
+  (16, 'comment', null, 2, 1, false, '2012-09-24 13:02:00')
 ;
 
 # --- !Downs
---
+++ webservice/test/models/VoteCounterSpec.scala
@@ -0,0 +1,45 @@
+package test.models
+
+import org.specs2.mutable._
+import org.specs2.mutable.After
+
+import play.api.test._
+import play.api.test.Helpers._
+
+class VoteCounterSpec extends org.specs2.mutable.Specification {
+
+  import models.VoteCounter
+  import models.Idea
+  import models.Comment
+
+  "Idea model VoteCounter" should {
+
+    "retrieve the votes for and idea" in {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        val Some(idea) = Idea.findById(1)
+
+        idea must not be none
+
+        idea.votes.pos must equalTo(3)
+        idea.votes.neg must equalTo(1)
+      }
+    }
+
+  }
+
+  "Comment model VoteCounter" should {
+
+    "retrieve the votes for a comment" in {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        val Some(comment) = Comment.findById(1)
+
+        comment must not be none
+        println(comment.votes.toString)
+        comment.votes.pos must equalTo(2)
+        comment.votes.neg must equalTo(1)
+      }
+    }
+
+  }
+
+}
