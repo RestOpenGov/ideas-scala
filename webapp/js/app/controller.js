b358f511df609577bbe7191aeeee973f9cef2002
Pablo H. Paladino <paladinop@gmail.com>
2012-09-24 21:44:25 -0300

    Comments on idea detail page

--- webapp/js/app/controller.js
+++ webapp/js/app/controller.js
@@ -30,16 +30,43 @@ function IdeaListCtrl($scope, $http) {
 
 function IdeaDetailCtrl($scope, $routeParams, $http) {
   $scope.ideaId = $routeParams.ideaId;
-  $scope.idea_endpoint = 'http://ideas-jugar.rhcloud.com/api/ideas/'+$scope.ideaId;
+  $scope.idea_endpoint = 'http://ideas-jugar.rhcloud.com/api/ideas/';
   $scope.idea = {};
+  $scope.comments = [];
   
   $scope.search = function(){
-    // Just in case the web service doesn't support X-Requested-With header
-    // delete $http.defaults.headers.common['X-Requested-With']
-    $http.get($scope.idea_endpoint).success(function(json) {
+
+    //IDEA
+    $http.get($scope.idea_endpoint+$scope.ideaId).success(function(json) {
       $scope.idea = json;
       $scope.idea.votes = votes();
       });
+
+    //COMMENTS
+    $http.get($scope.idea_endpoint+$scope.ideaId+'/comments').success(function(json) {
+      $scope.comments = json;
+      });
+
+  };
+
+  $scope.addComment = function(){
+    $commentBox = $('#comment-box').slideUp();
+    $commentBox.slideUp();
+    var data = {
+        "author": 1,
+        "comment": $scope.msg
+    };
+
+    $http({
+      url:$scope.idea_endpoint+$scope.ideaId+'/comment',
+      data : data,
+      method : 'POST',
+      headers : {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'},
+    }).success(function(){
+      $commentBox.find('textarea').val('');
+      $commentBox.slideDown();
+    });
+
   };
 
   $scope.search();
