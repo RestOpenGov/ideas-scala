4d2f1136155079e14784eca5c3f0ea39a7e6ed9a
opensas <opensas@gmail.com>
2012-10-01 12:27:17 -0300

    code style

--- webservice/conf/evolutions/default/1.sql
+++ webservice/conf/evolutions/default/1.sql
@@ -5,12 +5,12 @@
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
-  id             int(11) NOT NULL AUTO_INCREMENT,
-  nickname       varchar(100)  DEFAULT NULL,
-  name           varchar(100)  DEFAULT NULL,
-  email          varchar(100)  DEFAULT NULL,
-  avatar         varchar(100)  DEFAULT NULL,
-  created        timestamp     NULL DEFAULT NULL,
+  id          int(11)       NOT NULL AUTO_INCREMENT,
+  nickname    varchar(100)  DEFAULT NULL,
+  name        varchar(100)  DEFAULT NULL,
+  email       varchar(100)  DEFAULT NULL,
+  avatar      varchar(100)  DEFAULT NULL,
+  created     timestamp     NULL DEFAULT NULL,
 
   PRIMARY KEY (id),
   UNIQUE KEY user_uk_nickname (nickname),
