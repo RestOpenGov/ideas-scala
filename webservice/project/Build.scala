8800434fcb4b8cbec9e930323e13d714446a501f
opensas <opensas@gmail.com>
2012-09-30 21:04:54 -0300

    refs #27, include language detection libraries

--- webservice/project/Build.scala
+++ webservice/project/Build.scala
@@ -8,11 +8,14 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-    	"mysql" % "mysql-connector-java" % "5.1.18"
+      "mysql" % "mysql-connector-java" % "5.1.18",
+      "org.knallgrau.utils" % "textcat" % "1.0.1",
+      "com.cybozu.labs" % "langdetect" % "1.1-20120112"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here      
+      resolvers += "maven-us.nuxeo.org" at "https://maven-us.nuxeo.org/nexus/content/repositories/public"
     )
 
 }
