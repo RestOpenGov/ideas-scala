965929229afa1894b40413279c5444d9bcec23e6
opensas <opensas@gmail.com>
2012-10-15 10:39:35 -0300

    add an ErrorListException, to throw our error collections

---
+++ webservice/app/exceptions/ErrorListException.scala
@@ -0,0 +1,18 @@
+package exceptions
+
+import models.Error
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sas
+ * Date: 6/6/12
+ * Time: 11:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+case class ErrorListException(errors: List[Error]) extends RuntimeException
+
+object ErrorList {
+  def apply(errors: List[Error]): ErrorListException = ErrorList(errors)
+  def apply(error: Error): ErrorListException = ErrorList(List(error))
+}
