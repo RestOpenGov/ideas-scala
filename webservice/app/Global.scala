50a0868430b10d8917ce178cbccdd3971fe2ea93
Nicolás Melendez <nfmelendez@gmail.com>
2012-10-20 18:38:50 -0300

    Greating

--- webservice/app/Global.scala
+++ webservice/app/Global.scala
@@ -1,6 +1,7 @@
 import play.api._
 import play.api.mvc._
 import play.api.mvc.Results._
+import play.Logger
 
 import models.Error
 import formatters.json.ErrorFormatter._
@@ -35,7 +36,19 @@ object Global extends GlobalSettings {
   }
 
   override def onStart(app: Application) {
-    Logger.info("Ideas-ba is Starting...")
+    val greeting = 
+    """
+     _      _                                    ___    _____ 
+    (_)    ( )                                  (  _`\ (  _  )
+    | |   _| |   __     _ _   ___     ______    | (_) )| (_) |
+    | | /'_` | /'__`\ /'_` )/',__)   (______)   |  _ <'|  _  |
+    | |( (_| |(  ___/( (_| |\__, \              | (_) )| | | |
+    (_)`\__,_)`\____)`\__,_)(____/              (____/'(_) (_)
+                                                        
+    IDEASBA IS STARTING ...
+               ^^^^^^^^
+    """
+    Logger.info(greeting)
     val props = Props[NotificationActor].withRouter(RoundRobinRouter(nrOfInstances = 10))
     val a = Akka.system.actorOf(props, name = "notificationActor");
   }  
