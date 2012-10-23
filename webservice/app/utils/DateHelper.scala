210fdf3988c31a45339e0dd103aea84bed2f60a8
opensas <opensas@gmail.com>
2012-10-23 08:40:25 -0300

    refs #14 complete tests

--- webservice/app/utils/DateHelper.scala
+++ webservice/app/utils/DateHelper.scala
@@ -1,16 +1,18 @@
 package utils
 
-import java.util.{Date => JavaDate}
+import java.util.Date
 import java.util.Calendar
 
 object DateHelper {
 
   private lazy val calendar = Calendar.getInstance
 
-  def addSeconds(date: JavaDate, seconds: Int): JavaDate = {
+  def addSeconds(date: Date, seconds: Int): Date = {
     calendar.setTime(date)
     calendar.add(Calendar.SECOND, seconds)
     calendar.getTime
   }
+
+  def now: Date = new Date()
 
 }
--- webservice/test/services/security/UserSecuritySpec.scala
+++ webservice/test/services/security/UserSecuritySpec.scala
@@ -1,4 +1,4 @@
-package test.models
+package test.services.security
 
 import org.specs2.mutable._
 
@@ -11,7 +11,7 @@ import models.Error
 
 class UserSecuritySpec extends Specification with ErrorSpec {
 
-  import models.User
+  import models.{User, Identity}
   import models.User._
 
   "User.findByProviderId" should {
@@ -36,37 +36,130 @@ class UserSecuritySpec extends Specification with ErrorSpec {
 
   "User.findOrCreateFromProviderInfo" should {
 
+    import services.security.Social.retrieveSocialProviderInfo
+    import services.security.AccessToken
+
+    val accessToken = AccessToken("twitter", "valid twitter token")
+    val Some(providerInfo) = retrieveSocialProviderInfo(accessToken)
+
     "return the user if it already exists" in {
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
-        // TODO
-        1 must equalTo(1)
+
+        val beforeUsers = User.count()
+        val beforeIdentities = Identity.count()
+
+        val Right(originalUser) = User.findOrCreateFromProviderInfo(providerInfo)
+
+        val originalUsers = User.count()
+        val originalIdentities = Identity.count()
+
+        // a user and an identity were created
+        originalUsers must equalTo(beforeUsers + 1)
+        originalIdentities must equalTo(beforeIdentities + 1)
+
+        val Right(existingUser) = User.findOrCreateFromProviderInfo(providerInfo)
+        existingUser.id.get must equalTo(originalUser.id.get)
+
+        // no user nor identity were created
+        User.count() must equalTo(originalUsers)
+        Identity.count() must equalTo(originalIdentities)
+
       }
     }
 
     "create the user if it doesn't exists" in {
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
-        // TODO
-        1 must equalTo(1)
+
+        val usersBefore = User.count()
+        val identitiesBefore = Identity.count()
+
+        val Right(newUser) = User.findOrCreateFromProviderInfo(providerInfo)
+        newUser.nickname must equalTo(providerInfo.nickname)
+
+        User.count() must equalTo(usersBefore + 1)
+        Identity.count() must equalTo(identitiesBefore + 1)
+
+        val newIdentity = Identity.find(q = "user_id:%s".format(newUser.id.get))
+
+        newIdentity.size must equalTo(1)
       }
     }
 
-    "return an error if i has to create a user but another user with the same nickname exists" in {
-      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
-        // TODO
-        1 must equalTo(1)
-      }
-    }
+    // WEIRD COMPILE ERROR!!! compilation never ends
+    // if I have two operations on User ????
+    // 
+    // "return an error if it has to create a user but another user with the same nickname exists" in {
+    //   running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+
+    //     User(nickname = providerInfo.nickname).save must beRight
+
+    //     User.findOrCreateFromProviderInfo(providerInfo) must beLeft
+
+    //     todo
+    //   }
+    // }
 
   }
 
   "User.createFromProviderInfo" should {
 
+    import services.security.Social.retrieveSocialProviderInfo
+    import services.security.AccessToken
+
+    val accessToken = AccessToken("twitter", "valid twitter token")
+    val Some(providerInfo) = retrieveSocialProviderInfo(accessToken)
+
     "create the user and also the identity" in {
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
-        // TODO
-        1 must equalTo(1)
+
+        val usersBefore = User.count()
+        val identitiesBefore = Identity.count()
+
+        val Right(newUser) = User.createFromProviderInfo(providerInfo)
+        newUser.nickname must equalTo(providerInfo.nickname)
+
+        User.count() must equalTo(usersBefore + 1)
+        Identity.count() must equalTo(identitiesBefore + 1)
+
+        val newIdentity = Identity.find(q = "user_id:%s".format(newUser.id.get))
+
+        newIdentity.size must equalTo(1)
+
       }
     }
+
+    // WEIRD COMPILE ERROR!!! compilation never ends
+    // if I have two operations on User ????
+
+    // "return an error when trying to create an already existing user" in {
+    //   running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+
+    //     // create the user
+    //     User(nickname = providerInfo.nickname).save must beRight
+    //     // User.createFromProviderInfo(providerInfo) //must beRight
+    //     User.createFromProviderInfo(providerInfo) //must beRight
+
+    //     // val usersBefore = User.count()
+    //     // val identitiesBefore = Identity.count()
+
+    //     // // try to create another user
+    //     // User.createFromProviderInfo(providerInfo) //must beLeft
+
+    //     // User.createFromProviderInfo(providerInfo) must haveError.like {
+    //     //   case error => {
+    //     //     error.errorCode must equalTo(Error.REQUIRED)
+    //     //     error.field must equalTo("name")
+    //     //     error.message must contain("""campo "nombre" no puede estar vacío""")
+    //     //   }
+    //     // }
+
+    //     // User.count() must equalTo(usersBefore)
+    //     // Identity.count() must equalTo(identitiesBefore)
+
+    //     done
+
+    //   }
+    // }
 
   }
 
@@ -74,8 +167,16 @@ class UserSecuritySpec extends Specification with ErrorSpec {
 
     "generate a new application token and save it to the user" in {
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
-        // TODO
-        1 must equalTo(1)
+
+        val Some(originalUser) = User.findById(1)
+
+        originalUser.refreshApplicationToken must beRight.like {
+          case user => {
+            user.applicationToken must not equalTo(originalUser.applicationToken)
+            user.tokenExpiration.after(originalUser.tokenExpiration) must beTrue
+          }
+        }
+
       }
     }
 
@@ -85,8 +186,18 @@ class UserSecuritySpec extends Specification with ErrorSpec {
 
     "create a new user with a fresh application token" in {
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
-        // TODO
-        1 must equalTo(1)
+
+        import utils.DateHelper.{now, addSeconds}
+
+        val beforeNow = addSeconds(now, -1)    // one second before now
+
+        User(nickname="new user").save must beRight.like {
+          case user => {
+            user.applicationToken must not equalTo("")
+            user.tokenExpiration.after(beforeNow) must beTrue
+          }
+        }
+
       }
     }
 
