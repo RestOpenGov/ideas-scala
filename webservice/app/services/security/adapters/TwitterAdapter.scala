a7deadc2e33cbe8cdea3e204aa795c72f5a0a276
opensas <opensas@gmail.com>
2012-10-27 18:46:54 -0300

    add twitter, google and facebook adapters refs #14

--- webservice/app/services/security/adapters/TwitterAdapter.scala
+++ webservice/app/services/security/adapters/TwitterAdapter.scala
@@ -4,29 +4,38 @@ import play.api.libs.json.JsValue
 import services.security.AccessToken
 import services.security.IdentityProviderInfo
 
+// https://api.twitter.com/1/account/verify_credentials.json
+// authorization: OAuth oauth_access_token="<access_token>" 
+// curl -H 'authorization: OAuth oauth_access_token="twitter token"' https://api.twitter.com/1/account/verify_credentials.json
+// {
+// "token":"twitter token",
+// "provider":"twitter"
+// }
+
 object TwitterAdapter extends SocialAdapter {
   val provider = "twitter"
-  override def fetch(accessToken: AccessToken): Option[IdentityProviderInfo] = {
-    accessToken.token match {
-      case "valid twitter token" =>
-        Some(IdentityProviderInfo(
-          "twitter", "twitter.id",
-          "twitter.nickname", "twitter.name",
-          "twitter.email", "twitter.avatar"))
-      case _ => super.fetch(accessToken)
-    }
-  }
-  
-  override def defineBaseUri(token: String) = "https://api.twitter.com/1.1/account/verify_credentials.json"
-  override def defineQueryParams(token: String) = Map("skip_status" -> "true")
-    
+
+  override def defineBaseUri(token: String) = "https://api.twitter.com/1/account/verify_credentials.json"
+  override def defineHeaders(token: String) = Map(
+    "authorization" -> "OAuth oauth_access_token=\"%s\"".format(token)
+  )
+
   override def parseJsonResponse(json: JsValue): Option[IdentityProviderInfo] = {
     for {
-      id <- (json \ "id_str").asOpt[String]
-      username <- (json \ "screen_name").asOpt[String]
-      name <- (json \ "name").asOpt[String]
-      email <- Option("")
-      avatar <- (json \ "profile_image_url").asOpt[String]
-    } yield IdentityProviderInfo("twitter", id, username, name, email, avatar)
+      id        <- Some(((json \ "id").asOpt[Int].getOrElse(0)).toString)
+      username  <- (json \ "screen_name").asOpt[String]
+      name      <- (json \ "name").asOpt[String]
+      email     <- Some("")
+      avatar    <- (json \ "profile_image_url").asOpt[String]
+    } yield IdentityProviderInfo(provider, id, username, name, email, avatar)
   }
+
 }
+
+// to interactively test it on the console
+// new play.core.StaticApplication(new java.io.File("."))
+// val uri = "https://api.twitter.com/1/account/verify_credentials.json"
+// val headers = Seq( ("authorization", """OAuth oauth_access_token="twitter token"""") )
+// import play.api.libs.ws.WS
+// val resp = WS.url(uri).withHeaders(headers: _*).get().await.get.body
+// import play.api.libs.json.Json
