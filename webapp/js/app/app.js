f3e99cffdddff8a6e5120452aad33fc8d96b4771
Pablo H. Paladino <paladinop@gmail.com>
2012-11-09 22:07:59 -0300

    Revert "remove harcoded token, sorry!"
    This reverts commit 701eef3f76f63a8f5b8cb764330abd1cd8ee846f.

--- webapp/js/app/app.js
+++ webapp/js/app/app.js
@@ -40,7 +40,7 @@ ideasModule.run(function($rootScope,$http) {
     //TODO add header with 
 
     var token = SocialAuth.get('ideas-ba-token'), _headers={};
-
+    token="opensas";
     if (token) {
         _headers["Authorization"] = 'ideas-token='+token;
     }
