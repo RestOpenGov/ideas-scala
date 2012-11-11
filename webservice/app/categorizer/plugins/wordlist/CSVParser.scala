0ded27019b3915a5d3ec0f543983584792505f00
opensas <opensas@gmail.com>
2012-11-11 20:43:15 -0300

    refs #114

---
+++ webservice/app/categorizer/plugins/wordlist/CSVParser.scala
@@ -0,0 +1,11 @@
+package categorizer.plugins.wordlist
+
+import categorizer.SimpleToken
+import categorizer.SimpleTokenFormatter._
+
+object CSVParser {
+
+  // val csv = scala.io.Source.fromFile("conf/categorizer/csv/teatros.es.csv").getLines.toList
+
+  // val c2 = csv.slice(7,csv.size)
+}
